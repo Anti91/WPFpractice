49bfd0529afa83390672b8c14b34be11ed399cd5
Antal Nagy <nagyant91@gmail.com>
2018-02-08 13:06:14 +0100

    Style & Resource  first try

--- WPFsumApp/ViewModel/OperationViewModel.cs
+++ WPFsumApp/ViewModel/OperationViewModel.cs
@@ -15,6 +15,12 @@ namespace WPFsumApp.ViewModel
             set;
         }
 
+        public string LoremIpsumText
+        {
+            get;
+            set;
+        }
+
         public OperationViewModel()
         {
             OperationCollection = new List<string>()
@@ -24,6 +30,16 @@ namespace WPFsumApp.ViewModel
                 "*",
                 "/"
             };
+
+            LoremIpsumText =
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                                 "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                                 "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
+                                 "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit" +
+                                 " in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur " +
+                                 "sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt " +
+                                 "mollit anim id est laborum.";
+            
         }
     }
 }
